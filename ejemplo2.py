c548700d6e6d7bccd4b2a606bfe82e46868f5c54
Dayanna Flores <dayanna@gmail.com>
2023-11-28 12:10:45 -0300

    algunos ejemplos iniciales de python

--- ejemplo2.py
+++ ejemplo2.py
@@ -1,2 +1,31 @@
-var="Santiago"
+var="Hola Mundo"
 print (var[2:5])
+print("/n")
+a=10
+print(a)
+# este es código para trabajar números complejos,  esto sirve para estadísticas
+num=1+2j
+print(num)
+print(num.real)
+print(num.imag)
+"""
+esto es un comentarios
+de varias líneas en Python
+
+"""
+print(var)
+
+lev_var=len(var)
+print(lev_var)
+
+def mi_funcion(arg1, arg2):
+    print(arg1)
+    print(arg2)
+
+var1="Hola"
+var2="Adios"
+
+
+mi_funcion(arg1=var1,arg2=var2)    
+ 
+import this
